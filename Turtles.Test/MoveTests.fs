b082837903c6e5d2f7b90aff2eca6f0aa5e1fc70
bjartwolf <bjartnes@gmail.com>
2014-12-22 22:07:26 +0100

    init

---
+++ Turtles.Test/MoveTests.fs
@@ -0,0 +1,52 @@
+﻿module Turtles.Test
+
+open NUnit.Framework
+open FsCheck
+open FsUnit
+open FsCheck.NUnit
+open System 
+open Turtles
+
+[<TestFixture>] 
+type ``Given a LightBulb that has had its state set to true`` ()=
+    [<Test>] member x.
+     ``when I ask whether it is On it answers true.`` ()=
+            true |> should be True
+            
+[<Property>]
+let ``Moving 360n + 180 should move in opposite direction`` (n: int) =
+    let t0 = (1m, (0m, 0m))
+    let t1 = t0 
+                |> move 10m 
+    let t2 = t0 
+                |> turn (decimal(decimal(n)*decimal(2.0*Math.PI)))
+                |> turn (decimal Math.PI)
+                |> move 10m 
+    let _,(x,y) = t2
+    let t3 = 0m,(-x,-y)
+    isSamePosition'ish 10 t1 t3
+
+[<Property>]
+let ``Given any location, heading 0 moving 90 degrees and forward 4 times end up in same location`` (x: decimal) (y: decimal) = 
+    let t1: Turtle = 0m, (x/100m, y/100m)
+    let t2 = t1 |> move 10m |> turn (decimal (Math.PI/2.0))
+               |> move 10m |> turn (decimal (Math.PI/2.0))
+               |> move 10m |> turn (decimal (Math.PI/2.0))
+               |> move 10m 
+    isSamePosition'ish 4 t1 t2
+
+[<Property>]
+let ``Given any location and heading moving 90 degrees and forward 4 times end up in same location to three digits`` (x: decimal) (y: decimal) = 
+    let t: Turtle = 1m, (x/100m, y/100m)
+    let t1 = t |> move 0m // Just to get the rounding of the move function
+    let t2 = t |> move 10m |> turn (decimal (Math.PI/2.0))
+               |> move 10m |> turn (decimal (Math.PI/2.0))
+               |> move 10m |> turn (decimal (Math.PI/2.0))
+               |> move 10m 
+    isSamePosition'ish 3 t1 t2
+
+[<Property>]
+let ``Moving a multiple of 360 should move forward the same`` (n: int) =
+    let t1: Turtle = move 10m (1m, (0m, 0m)) 
+    let t2: Turtle = move 10m (1m+decimal(decimal(n)*decimal(2.0*Math.PI)), (0m, 0m)) 
+    isSamePosition'ish 3 t1 t2
---
+++ Turtles/Program.fs
@@ -0,0 +1,51 @@
+﻿module Turtles
+open System
+
+type Dir = decimal 
+type Position = decimal*decimal
+type Turtle = Dir * Position
+type Length = decimal 
+type Angle = decimal 
+
+let roundN (nrOfDecimals: int)(value:decimal) = Math.Round(value, nrOfDecimals)
+let round5 = roundN 5 
+let round10 = roundN 10 
+
+let dcos (value: decimal) : decimal =
+   decimal (cos (double value))
+
+let dsin (value: decimal) : decimal =
+   decimal (sin(double value))
+
+let turn (a:Angle) (t: Turtle): Turtle = 
+    let dir,pos = t 
+    let dir' = dir + a
+    (dir', pos) 
+
+let round (digits:int)(t:Turtle): Turtle =
+    let dir,(x,y) = t 
+    let rounder = roundN digits
+    let pos' = (rounder x,rounder y)
+    (dir, pos') 
+
+let isSamePosition'ish (digits:int)(t1: Turtle)(t2: Turtle): bool = 
+    let t1' = round digits t1 
+    let t2' = round digits t2 
+    let _, (x1,y1) = t1'
+    let _, (x2,y2) = t2'
+    x1 = x2 && y1 = y2 
+
+let move (l:Length)(t: Turtle) : Turtle = 
+    let dir,(x,y) = t 
+    let x' = x + l * dcos dir
+    let y' = y + l * dsin dir
+    let pos' = (x',y')
+    (dir, pos') 
+
+[<EntryPoint>]
+let main argv = 
+    let t1 : Turtle = (10m, (10m,10m))
+    let t2 = move 10m t1 
+    printfn "%A" t2 
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
