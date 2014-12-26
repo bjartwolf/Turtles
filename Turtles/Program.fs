71018e4483c3b6f99557bde5f47db42b5b905a4f
bjartwolf <bjartnes@gmail.com>
2014-12-26 12:09:09 +0100

    Added a  painter in winforms to draw turtles

--- Turtles/Program.fs
+++ Turtles/Program.fs
@@ -1,10 +1,22 @@
 ﻿open Turtles
 open System 
+open Painter 
+
+let step (t: Turtle) : Turtle =
+  t 
+    |> move 1.0 
+    |> turnDeg 1.0
+    |> move 1.0 
+
+let rec play (t: Turtle) (turns: int) : Turtle= 
+   if turns = 0 then painter.Post(Stop)
+                     printfn "%A" t 
+                     t
+   else play (step t) (turns - 1)
 
 [<EntryPoint>]
 let main argv = 
-    let t1 : Turtle = (10.0, (10.0,10.0))
-    let t2 = move 10.0 t1 
-    printfn "%A" t2 
+    let t1 : Turtle = (0.0, (200.0,200.0))
+    let t2 = play t1 (360*10)
     Console.ReadLine() |> ignore
     0 // return an integer exit code
--- Turtles/Turtle.fs
+++ Turtles/Turtle.fs
@@ -1,40 +1,52 @@
 ﻿module Turtles
 open System
+open Painter 
 
 type Dir = double // The direction of the turtle
 type Position = double*double // The x-y position of the turtle
 type Turtle = Dir * Position // A turtle state is its direction and position
 type Length = double // How long the turtle should move
+// Should make this unit of work?
 type Angle = double // How much the turtle should turn
+type Degrees = double // How much the turtle should turn in degrees
 
-let roundN (nrOfdoubles: int)(value:double) = Math.Round(value, nrOfdoubles)
+let roundN (nrOfdoubles: int) (value:double) = Math.Round(value, nrOfdoubles)
 let round5 = roundN 5 
 let round10 = roundN 10 
 
-let turn (a:Angle) (t: Turtle): Turtle = 
+// Turns the turtle a (radians) degrees
+let turn (a:Angle) (t: Turtle) : Turtle = 
     let dir,pos = t 
     let dir' = dir + a
     (dir', pos) 
 
+// Turns the turtle a (radians) degrees
+let turnDeg (a:Degrees) (t: Turtle) : Turtle = 
+    turn (a * double (Math.PI/180.0)) t
+
 let turn60 = turn (double (Math.PI/3.0))
 let turn90 = turn (double (Math.PI/2.0))
 
-let round (digits:int)(t:Turtle): Turtle =
-    let dir,(x,y) = t 
+// Rounds of the turtle position
+let round (digits:int) (t:Turtle) : Turtle =
+    let dir,(x,y) = t
     let rounder = roundN digits
     let pos' = (rounder x,rounder y)
-    (dir, pos') 
+    (dir, pos')
 
-let isSamePosition'ish (digits:int)(t1: Turtle)(t2: Turtle): bool = 
-    let t1' = round digits t1 
-    let t2' = round digits t2 
+let isSamePosition'ish (digits:int) (t1: Turtle) (t2: Turtle): bool =
+    let t1' = round digits t1
+    let t2' = round digits t2
     let _, (x1,y1) = t1'
     let _, (x2,y2) = t2'
-    x1 = x2 && y1 = y2 
+    x1 = x2 && y1 = y2
 
+// Move in the current direction
 let move (l:Length)(t: Turtle) : Turtle = 
     let dir,(x,y) = t 
     let x' = x + l * cos dir
     let y' = y + l * sin dir
     let pos' = (x',y')
-    (dir, pos') 
+    let msg : line = (x,y),(x',y') 
+    painter.Post(Line msg)
+    (dir, pos')
