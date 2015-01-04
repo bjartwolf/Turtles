a7de1359db270374baf306a23bb7671f1d418be4
bjartwolf <bjartnes@gmail.com>
2015-01-04 15:09:48 +0100

    scaling right

--- TurtlePainterPlay/Program.fs
+++ TurtlePainterPlay/Program.fs
@@ -42,13 +42,6 @@ let main argv =
     RenderLoop.Run(form, fun _ ->
             d2DRenderTarget.BeginDraw()
             d2DRenderTarget.Clear(new Nullable<SharpDX.Color4>(SharpDX.Color.Black.ToColor4()))
-//            let zeroMove = ((0.0f,0.0f),(0.0f,0.0f))
-//            let printNewSeq (seq1:seq<Line*Turtle*bool>) = 
-//                let printer (lt: Line*Turtle*bool)= 
-//                    let (l,t,_) = lt
-//                    let ((x1,y1),(x2,y2)) = l
-//                    d2DRenderTarget.DrawLine(new Vector2(x1,y1),new Vector2(x2,y2), pinkBrush,0.3f) 
-//                Seq.iter printer seq1; 
             let printLines (lines:seq<Line option>) = 
                 let printLine (l: Line) = 
                     let ((x1,y1),(x2,y2)) = l 
@@ -57,24 +50,9 @@ let main argv =
                     match line with 
                         | None -> ()
                         | Some(l) -> printLine l 
-//            let printSeq (seq1:seq<Line*Turtle>) = 
-//                let printer (lt: Line*Turtle)= 
-//                    let (l,t) = lt
-//                    let ((x1,y1),(x2,y2)) = l
-//                    d2DRenderTarget.DrawLine(new Vector2(x1,y1),new Vector2(x2,y2), pinkBrush,0.3f) 
-//                Seq.iter printer seq1; 
-//            Seq.unfold (myNewPolyTurtle 242.0) (zeroMove, (0.0, (500.0, 500.0)),false) |> printNewSeq 
-//            Seq.unfold (myPolyTurtle -291.0) (zeroMove, (0.0, (500.0, 500.0))) |> printSeq 
-            for x in 1 .. 24 do // 24*15 = 360
+            for x in 0 .. 24 do // 24*15 = 360
                 for y in 1 .. 15 do
                       yieldTurtle (x*15+y) (0.0, (float (x+1) * 50.0,float (y-1) *50.0)) |> printLines
-//                    Seq.unfold (myNewPolyTurtle (float (x*15+y))) (zeroMove, (0.0, (float (x+1) * 50.0,float (y-1) *50.0)),false) |> printNewSeq 
-//                    Seq.unfold (myNewPolyTurtle -291.0) (zeroMove, (0.0, (500.0, 500.0)),false) |> printNewSeq 
-//            for x in 0 .. 23 do // 24*15 = 360
-//            for x in 0 .. 18 do // 24*15 = 360
-//                for y in 1 .. 15 do
-////                    Seq.unfold (myTurtle (x*15+y)) (zeroMove, (0.0, (float (x+1) * 50.0,float (y-1) *50.0))) |> printSeq 
-//                    Seq.unfold (myPolyTurtle (float (x*15+y))) (zeroMove, (0.0, (float (x+1) * 50.0,float (y-1) *50.0))) |> printSeq 
             d2DRenderTarget.EndDraw()
             (!swapChain).Present(0, PresentFlags.None)
         )
--- Turtles/Turtle.fs
+++ Turtles/Turtle.fs
@@ -54,38 +54,7 @@ type Lines = Line list
 let step (t: Turtle) (degreesToTurn: double) : Turtle =
   t 
     |> turnDeg degreesToTurn 
-    |> move 1.0 
-//    |> move (max (min degreesToTurn 7.0) 10.0)
-
-let myNewPolyTurtle (turnDeg: float) (l: Line, t: Turtle, shouldMoveDoubleAngle: bool) = 
-   let dir, _= t 
-   let distFromPi = Math.Abs(dir % (2.0*Math.PI))
-   let zeroMove = ((0.0f,0.0f),(0.0f,0.0f))
-   let lineIsZero = l = zeroMove 
-   if  not lineIsZero && (not shouldMoveDoubleAngle) && (distFromPi < 0.0001 || distFromPi > (2.0*Math.PI-0.0001)) then 
-//   if  not lineIsZero && dir > Math.PI*2.0 then 
-        None 
-   else
-        let t' = step t (if shouldMoveDoubleAngle then turnDeg*2.0 else turnDeg)  
-        let _,(x,y) = t
-        let _,(x',y') = t'
-        let lastMove = ((single x,single y),(single x',single y'))
-        Some((lastMove,t, shouldMoveDoubleAngle),(lastMove, t', not shouldMoveDoubleAngle))
-
-let myPolyTurtle (turnDeg: float) (l: Line, t: Turtle) = 
-   let dir, _= t 
-   let distFromPi = Math.Abs(dir % (2.0*Math.PI))
-   let zeroMove = ((0.0f,0.0f),(0.0f,0.0f))
-   let lineIsZero = l = zeroMove 
-   if  not lineIsZero && (distFromPi < 0.01 || distFromPi > (2.0*Math.PI-0.01)) then 
-//   if  not lineIsZero && dir > Math.PI*2.0 then 
-        None 
-   else
-        let t' = step t turnDeg  
-        let _,(x,y) = t
-        let _,(x',y') = t'
-        let lastMove = ((single x,single y),(single x',single y'))
-        Some((lastMove,t),(lastMove, t'))
+    |> move (0.4 * degreesToTurn)
 
 let turtleLine (t:Turtle) (t': Turtle) : Line = let _,(x,y) = t
                                                 let _,(x',y') = t'
@@ -98,8 +67,6 @@ let rec yieldTurtle (edges: int) (t: Turtle): seq<Line option> =
    seq {
         let t' = step t turnDeg  
         yield Some (turtleLine t t')
-////        let t' = step t (2.0*turnDeg) 
-//        yield Some (turtleLine t t')
         let dir, _= t'
         let distFromPi = Math.Abs(dir % (2.0*Math.PI)) in 
         if (distFromPi < 0.01 || distFromPi > (2.0*Math.PI-0.01)) then 
@@ -107,19 +74,3 @@ let rec yieldTurtle (edges: int) (t: Turtle): seq<Line option> =
         else 
             yield! (yieldTurtle edges t')
     }
-
-let myTurtle (edges: int) (l: Line, t: Turtle) = 
-   let turnDeg = 360.0/ (float edges)
-   let dir, _= t 
-   let distFromPi = Math.Abs(dir % (2.0*Math.PI))
-   let zeroMove = ((0.0f,0.0f),(0.0f,0.0f))
-   let lineIsZero = l = zeroMove 
-//   if  not lineIsZero && (distFromPi < 0.01 || distFromPi > (2.0*Math.PI-0.01)) then 
-   if  not lineIsZero && dir > Math.PI*2.0 then 
-        None 
-   else
-        let t' = step t turnDeg  
-        let _,(x,y) = t
-        let _,(x',y') = t'
-        let lastMove = ((single x,single y),(single x',single y'))
-        Some((lastMove,t),(lastMove, t'))
