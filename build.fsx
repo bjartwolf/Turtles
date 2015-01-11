85430590f6bb35653cce4f98432e9632c0d9a775
bjartwolf <bjartnes@gmail.com>
2015-01-11 18:23:14 +0100

    fake working

--- build.fsx
+++ build.fsx
@@ -1,5 +1,5 @@
 // include Fake libs
-#r "tools/FAKE/FakeLib.dll"
+#r "packages/FAKE/tools/FakeLib.dll"
 
 open Fake
 
@@ -7,8 +7,10 @@ open Fake
 let buildDir  = "./build/"
 let testDir   = "./test/"
 
-//let testReferences = !! "src/test/**/*.csproj"
-let testReferences = !! "Turtles.Test/*.csproj"
+// Filesets
+let appReferences  = !! "src/Turtle\*.fsproj"
+
+let testReferences = !! "src/Test/**/*.fsproj"
 
 // Targets
 Target "Clean" (fun _ -> 
@@ -16,21 +18,6 @@ Target "Clean" (fun _ ->
 )
 
 Target "BuildApp" (fun _ ->
-    CreateCSharpAssemblyInfo "./src/app/Calculator/Properties/AssemblyInfo.cs"
-        [Attribute.Title "Calculator Command line tool"
-         Attribute.Description "Sample project for FAKE - F# MAKE"
-         Attribute.Guid "A539B42C-CB9F-4a23-8E57-AF4E7CEE5BAA"
-         Attribute.Product "Calculator"
-         Attribute.Version version
-         Attribute.FileVersion version]
-
-    CreateCSharpAssemblyInfo "./src/app/CalculatorLib/Properties/AssemblyInfo.cs"
-        [Attribute.Title "Calculator library"
-         Attribute.Description "Sample project for FAKE - F# MAKE"
-         Attribute.Guid "EE5621DB-B86B-44eb-987F-9C94BCC98441"
-         Attribute.Product "Calculator"
-         Attribute.Version version
-         Attribute.FileVersion version]
 
     // compile all projects below src/app/
     MSBuildRelease buildDir "Build" appReferences
@@ -43,46 +30,24 @@ Target "BuildTest" (fun _ ->
 )
 
 Target "NUnitTest" (fun _ ->  
-    !! (testDir + "/NUnit.Test.*.dll")
+    !! (testDir + "/Turtles.Test.dll")
         |> NUnit (fun p -> 
             {p with
                 DisableShadowCopy = true; 
                 OutputFile = testDir + "TestResults.xml"})
 )
 
-Target "xUnitTest" (fun _ ->  
-    !! (testDir + "/xUnit.Test.*.dll")
-        |> xUnit (fun p -> 
-            {p with 
-                ShadowCopy = false;
-                HtmlOutput = true;
-                XmlOutput = true;
-                OutputDir = testDir })
-)
-
-Target "FxCop" (fun _ ->
-    !! (buildDir + "/**/*.dll") 
-        ++ (buildDir + "/**/*.exe")
-        |> FxCop (fun p -> 
-            {p with                     
-                ReportFileName = testDir + "FXCopResults.xml";
-                ToolPath = fxCopRoot})
-)
-
-Target "Deploy" (fun _ ->
-    !! (buildDir + "/**/*.*") 
-        -- "*.zip" 
-        |> Zip buildDir (deployDir + "Calculator." + version + ".zip")
-)
+//Target "Deploy" (fun _ ->
+//    !! (buildDir + "/**/*.*") 
+//        -- "*.zip" 
+//        |> Zip buildDir (deployDir + "Calculator." + version + ".zip")
+//)
 
 // Build order
 "Clean"
   ==> "BuildApp"
   ==> "BuildTest"
-  ==> "FxCop"
   ==> "NUnitTest"
-  =?> ("xUnitTest",hasBuildParam "xUnitTest")  // only if FAKE was called with parameter xUnitTest
-  ==> "Deploy"
 
 // start build
-RunTargetOrDefault "Deploy"
+RunTargetOrDefault "NUnitTest"
