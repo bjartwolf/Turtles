5aa06ff03a2a0dcfe276f6c7fa7d31284b154dc7
bjartwolf <bjartnes@gmail.com>
2015-01-12 07:28:58 +0100

    build script

--- build.cmd
+++ build.cmd
@@ -5,5 +5,4 @@ cls
 if not exist "packages\FAKE\tools\Fake.exe" (
     ".nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion" "-Prerelease"
 )
-dir 
-"packages\FAKE\tools\Fake.exe" "build.fsx"
+"tools\FAKE\tools\Fake.exe" "build.fsx"
