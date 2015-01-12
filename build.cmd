a51b1099020c0179fbe89c91437c0bf0c6565dc9
bjartwolf <bjartnes@gmail.com>
2015-01-12 07:19:38 +0100

    adding nuget

---
+++ build.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+cls
+
+if not exist "packages\FAKE\tools\Fake.exe" (
+    ".nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion" "-Prerelease"
+)
+
+packages\FAKE\tools\Fake.exe build.fsx
